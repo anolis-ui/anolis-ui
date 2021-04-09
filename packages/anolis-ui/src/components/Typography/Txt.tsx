877bc9c457264367ad03789382c4584cf01c809c
Denis Homolik <alfonz@homolik.cz>
2021-04-09 16:47:08 +0200

    Fix Txt

--- packages/anolis-ui/src/components/Typography/Txt.tsx
+++ packages/anolis-ui/src/components/Typography/Txt.tsx
@@ -4,7 +4,7 @@ import { anolisComponent } from "utils/anolisComponent";
 import { useComponentTheme } from "hooks/useComponentTheme";
 
 interface TxtProps extends PseudoProp {
-  t: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "lead" | "a";
+  t: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "lead";
 }
 
 export const Txt = anolisComponent<"p", TxtProps>("p", ({ v, t, ...props }, ref) => {
