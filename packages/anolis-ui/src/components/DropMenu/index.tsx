345f50b8e7376576538a39b0f7ccc8970b128405
tomasnyvlt <nnyvlt@gmail.com>
2021-08-04 16:07:18 +0200

    Add Dropdown to index

--- packages/anolis-ui/src/components/DropMenu/index.tsx
+++ packages/anolis-ui/src/components/DropMenu/index.tsx
@@ -1,10 +1,7 @@
 import { Menu } from "@reach/menu-button";
-import { ComplementProps } from "components/Complement";
 import { FC } from "react";
 
 export * from "./theme";
-
-export type DropMenuItemProps = ComplementProps;
 
 export const DropMenu: FC = ({ children }) => {
   return (
--- packages/anolis-ui/src/index.ts
+++ packages/anolis-ui/src/index.ts
@@ -12,6 +12,10 @@ export * from "./components/TextLink";
 export * from "./components/Label";
 export * from "./components/Input";
 export * from "./components/Modal";
+export * from "./components/DropMenu";
+export * from "./components/DropMenu/DropdownMenuButton";
+export * from "./components/DropMenu/DropdownMenuList";
+export * from "./components/DropMenu/DropdownMenuItem";
 
 // Unthemed
 export * from "./components/Icon";
